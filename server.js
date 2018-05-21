6eb43cd8e0049f282f6e06e1c4284692bdd7287f
chagoy <angelochagoy@gmail.com>
2018-05-21 12:18:37 -0700

    completed adding search

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 'use strict';
 
-// Load array of notes
+const express = require('express');
+
+const app = express();
+
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+app.get('/api/notes', (req, res) => {
+    let query = req.query.searchTerm;
+    // console.log(query);
+
+    const searched = data.filter(function(item) {
+        return item.title.includes(query) || item.content.includes(query);
+    });
+
+    res.json(query ? searched : data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    let note = data.find(function(note){
+        return note.id == req.params.id;
+    });
+    res.json(note);
+});
+
+app.listen(8080, function() {
+    console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+    console.error(err);
+});
