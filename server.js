70d10d721f334782cba08a537f6b7a93804a44cd
chagoy <angelochagoy@gmail.com>
2018-05-22 13:26:59 -0700

    fixed error handling

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api/notes/:id', (req, res, next) => {
         if (item) {
             res.json(item);
         } else {
-            console.log('not found')
+            next();
         }
     });
 });
@@ -74,18 +74,18 @@ app.get('/boom', (req, res, next) => {
 });
 
 
+app.use(function(req, res, next) {
+    const err = new Error('not found');
+    err.status = 404;
+    next(err);
+});
+
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
         message: err.message,
         error: err
     });
-});
-
-app.use(function(req, res, next) {
-    var err = new Error('Not found');
-    err.status = 404;
-    res.status(404).json({message: 'Not found'});
 });
 
 app.listen(PORT, function() {
